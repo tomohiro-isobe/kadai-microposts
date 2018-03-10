57401125383d15d4cfe545c99afca9b7c0ac6975
tomohiro.isobe <ueda@co.jp>
2018-03-10 14:01:07 +0000

    user pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,7 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+} );
+
