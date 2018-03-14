5d918272be2a0f19e8b9012415f1d7f6c6d0ae5d
tomohiro.isobe <ueda@co.jp>
2018-03-14 14:09:15 +0000

    follow, unfollow

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -8,7 +8,7 @@
                     <h3 class="panel-title">{{ $user->name }}</h3>
                 </div>
                 <div class="panel-body">
-                <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                    <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
                 </div>
             </div>
             @include('user_follow.follow_button', ['user' => $user])
@@ -19,9 +19,7 @@
                 <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
                 <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
             </ul>
-            @if (count($microposts) > 0)
-                @include('microposts.microposts', ['microposts' => $microposts])
-            @endif
+            @include('users.users', ['users' => $users])
         </div>
     </div>
 @endsection
